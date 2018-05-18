b1ed42facc7e242651e7339d0de44c64073547c0
Dipankar Dutta <dip@dipankar-mac.mynet>
2018-05-19 00:23:52 +0100

    fx

--- apps/books/genarate.py
+++ apps/books/genarate.py
@@ -31,11 +31,11 @@ div.code{    white-space: pre;
     font-size: 14px;
     letter-spacing: 1;
     line-height: 0.8;}
-div.x3{     margin-bottom: 25px;
+div.x3{     margin-bottom: 10px;
     text-align: justify;
 }
 ol,ul{    line-height: 1.3;
-        margin-bottom: 25px;}
+        margin-bottom: 35px;}
     </style>
 </head>
 <body>
@@ -57,39 +57,39 @@ block_start = False
 num_start = False
 code_start = False;
 for line in content:
-  if block_start and not line.startswith("* "):
-    output.append("</ul>")
-    block_start = False
-  if num_start and not line.startswith("1. "):
-    output.append("</ol>")
-    num_start = False
+    if block_start and not line.startswith("- "):
+        output.append("</ul>")
+        block_start = False
+    if num_start and not line.startswith("1. "):
+        output.append("</ol>")
+        num_start = False
 
-  if line.startswith("## "):
-    output.append("<div class='x1'>"+line[3:].strip()+"</div>")
-  elif line.startswith("# "):
-    output.append("<div class='x2'>"+line[2:].strip()+"</div>")
-  elif line.startswith("* "):
-    if block_start == False:
-      block_start = True;
-      output.append("<ul>")
-    output.append("<li>"+line[2:].strip()+"</li>")
-  elif line.startswith("1. "):
-    if num_start == False:
-      num_start = True;
-      output.append("<ol>")
-    output.append("<li>"+line[3:].strip()+"</li>")
-  elif line.startswith("{{{"):
-    code_start = True
-    output.append("<div class='code'>")
-  elif line.startswith("}}}"):
-    output.append("</div>")
-    code_start = False
-  else:
-    if not code_start and not num_start and not block_start :
-      output.append("<div class='x3'>"+line.strip()+"</div>")
+    if line.startswith("## "):
+        output.append("<div class='x1'>"+line[3:].strip()+"</div>")
+    elif line.startswith("# "):
+        output.append("<div class='x2'>"+line[2:].strip()+"</div>")
+    elif line.startswith("- "):
+        if block_start == False:
+            block_start = True;
+            output.append("<ul>")
+        output.append("<li>"+line[2:].strip()+"</li>")
+    elif line.startswith("1. "):
+        if num_start == False:
+            num_start = True;
+            output.append("<ol>")
+        output.append("<li>"+line[3:].strip()+"</li>")
+    elif line.startswith("{{{"):
+        code_start = True
+        output.append("<div class='code'>")
+    elif line.startswith("}}}"):
+        output.append("</div>")
+        code_start = False
     else:
-        if line.strip():
-            output.append(es(line))
+        if not code_start and not num_start and not block_start :
+            output.append("<div class='x3'>"+line.strip()+"</div>")
+        else:
+            if line.strip():
+                output.append(es(line))
 
 output = "\n".join(output)
 
